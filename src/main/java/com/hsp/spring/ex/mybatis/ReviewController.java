dfbbebfd2c46dbe146b97836a819f8ea908894e3
DESKTOP-I633S8U\user <93star2@gmail.com>
2023-02-24 18:02:39 +0900

    [박현석] - Mybatis INSERT 예제

--- src/main/java/com/hsp/spring/ex/mybatis/ReviewController.java
+++ src/main/java/com/hsp/spring/ex/mybatis/ReviewController.java
@@ -10,13 +10,14 @@ import com.hsp.spring.ex.mybatis.bo.ReviewBO;
 import com.hsp.spring.ex.mybatis.model.Review;
 
 @Controller
+@RequestMapping("/mybatis")
 public class ReviewController {
 
 	@Autowired
 	private ReviewBO reviewBO;
 	
 	// 전달받은 id와 일치하는 데이터 조회 후 그 결과를 json으로 response에 담기
-	@RequestMapping("/mybatis/ex01")
+	@RequestMapping("/ex01")
 	@ResponseBody
 	public Review review(
 			@RequestParam("id")int id
@@ -29,5 +30,36 @@ public class ReviewController {
 		
 		return review;
 	}
+	
+	
+	// 새로운 리뷰를 등록하는 기능
+	@RequestMapping("/ex02")
+	@ResponseBody
+	public String addReview() {
+		// 4, "콤비네이션피자", "김인규", 4.5, "할인도 많이 받고 잘 먹었어요"
+//		int count =  reviewBO.addReview(4, "콤비네이션피자", "김인규", 4.5, "할인도 많이 받고 잘 먹었어요");
+		
+		Review review = new Review();
+		review.setStoreId(2);
+		review.setMenu("뿌링클");
+		review.setUserName("김인규");
+		review.setPoint(4.0);
+		review.setReview("역시 뿌링클은 진리입니다!");
+		
+		int count = reviewBO.addReviewByObject(review);
+		
+		// 실행 결과 : 1
+		return "실행 결과 : " + count;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
